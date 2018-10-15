c392b6543c9881cf6256c5ef43a2db4be5652b68
wangxinglong007 <15373068+wangxinglong007@users.noreply.github.com>
2018-10-15 17:30:25 +0800

    Update RMQrecive.py
    update redis message

--- WingOn/requestBody/RMQ/RMQrecive.py
+++ WingOn/requestBody/RMQ/RMQrecive.py
@@ -6,12 +6,12 @@ import redis
 from Rsa_encrypt.js import rsa_encryption_data
 
 credentials = pika.PlainCredentials('admin', 'admin')
-parameters = pika.ConnectionParameters('172.18.21.189', 5672, 'TicketTest', credentials)
+parameters = pika.ConnectionParameters('xxxx', 'port', 'xxxxTest', credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 channel.queue_declare(queue='AutoPayQueue')
 print '[*] Waiting for messages. To exit press CTRL+C'
-r = redis.Redis(host='172.18.21.104', port=6379)
+r = redis.Redis(host='xxx', port=port)
 
 
 def callback(ch, method, properties, body):
